23efc33bde71c3b3eee17069e031a4971417718f
ciaa <154084369+ciaadcia@users.noreply.github.com>
2026-09-15 18:28:35 +0700

    Update Coin.cs

--- Coin.cs
+++ Coin.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Coin : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
-    {
-
-        Player player = other.GetComponent<Player>();
-
+    void OnTriggerEnter(Collider other) {
+ 
+        CoinsCounter coins = other.GetComponent<CoinsCounter>();
+ 
         //The number of coins is updated
-        player.CollectCoins();
-
+        coins.CollectCoins();
+ 
         //The coin that was collected is destroyed
         Destroy(gameObject);
     }
-
 }
-
